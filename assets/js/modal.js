e0add7b7a68886a302d6bd76682dd1827b45f787
fpdlrk <61310220+fpdlrk@users.noreply.github.com>
2023-12-17 19:46:20 +0900

    modified:   assets/css/btn.css 	modified:   assets/css/color.css 	modified:   assets/css/common.css 	modified:   assets/css/comp.css 	modified:   assets/css/layout.css 	modified:   assets/css/reset.css 	new file:   assets/img/ico/d/Ico_sh.svg 	new file:   assets/img/ico/d/ico_chk.svg 	new file:   assets/img/ico/d/ico_rad.svg 	new file:   assets/img/ico/d/ico_unChk.svg 	new file:   assets/img/ico/d/ico_unRad.svg 	new file:   assets/img/ico/w/ci.svg 	new file:   assets/img/ico/w/ci_txt.svg 	modified:   assets/js/modal.js 	deleted:    "componts/\353\241\234\354\247\201/index.htm" 	modified:   element/elemant.htm 	modified:   layout/layout.htm 	new file:   login/index.htm 	new file:   "\352\263\240\354\240\225\355\225\255\353\252\251/\353\262\204\355\212\274\354\204\244\354\240\225/index.htm" 	new file:   "\352\263\240\354\240\225\355\225\255\353\252\251/\353\262\204\355\212\274\354\204\244\354\240\225/view.htm" 	new file:   "\352\263\240\354\240\225\355\225\255\353\252\251/\354\204\244\353\254\270id/index.htm" 	new file:   "\352\263\240\354\240\225\355\225\255\353\252\251/\354\204\244\353\254\270id/view.htm" 	new file:   "\352\263\240\354\240\225\355\225\255\353\252\251/\354\204\244\353\254\270\354\240\234\353\252\251/index.htm" 	new file:   "\352\263\240\354\240\225\355\225\255\353\252\251/\354\204\244\353\254\270\354\240\234\353\252\251/view.htm" 	new file:   "\352\263\240\354\240\225\355\225\255\353\252\251/\354\247\204\355\226\211\354\234\250\353\260\224/index.htm" 	new file:   "\352\263\240\354\240\225\355\225\255\353\252\251/\354\247\204\355\226\211\354\234\250\353\260\224/view.htm" 	new file:   "\354\203\201\353\213\250\353\262\204\355\212\274\353\223\244/\353\241\234\354\247\201/index.htm" 	renamed:    "componts/\353\241\234\354\247\201/view.htm" -> "\354\203\201\353\213\250\353\262\204\355\212\274\353\223\244/\353\241\234\354\247\201/view.htm" 	renamed:    "componts/\354\212\244\355\203\200\354\235\274/index.htm" -> "\354\203\201\353\213\250\353\262\204\355\212\274\353\223\244/\354\204\244\354\240\225/index.htm" 	new file:   "\354\203\201\353\213\250\353\262\204\355\212\274\353\223\244/\354\212\244\355\203\200\354\235\274/index.htm" 	renamed:    "componts/\354\212\244\355\203\200\354\235\274/view.htm" -> "\354\203\201\353\213\250\353\262\204\355\212\274\353\223\244/\354\212\244\355\203\200\354\235\274/view.htm" 	new file:   "\354\203\201\353\213\250\353\262\204\355\212\274\353\223\244/\354\235\264\353\240\245/index.htm"

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,74 +1,124 @@
-const ModalOpen = function (popId, title, msg, type, okFnc, cancleFnc, { ...rest }) {
+const ModalOpen = function (
+  popId,
+  title,
+  msg,
+  type,
+  okFnc,
+  cancleFnc,
+  ...rest
+) {
+  class Modal {
+    constructor(
+      popId,
+      title,
+      msg,
+      type,
+      okCallBack,
+      cancleCallBack,
+      { ...rest }
+    ) {
+      this.popId = popId;
+      this.title = title;
+      this.msg = msg;
+      this.type = type;
+      this.okCallBack = okCallBack == "" ? this.ok : okCallBack;
+      this.cancleCallBack = cancleCallBack == "" ? this.cancle : cancleCallBack;
+      this.opt = { ...rest };
+    }
 
-	class Modal {
-		constructor(popId, title, msg, type, okCallBack, cancleCallBack, { ...rest }) {
-			this.popId = popId;
-			this.title = title;
-			this.msg = msg;
-			this.type = type;
-			this.okCallBack = okCallBack == '' ? this.ok : okCallBack;
-			this.cancleCallBack = cancleCallBack == '' ? this.cancle : cancleCallBack;
-			//this.opt = rest;
-		}
+    open() {
+      let html = "";
+      html += '<div id="' + this.popId + '" class="commPopup _tp_alert">';
+      if (this.title != null && this.title != "") {
+        html += '<div class="pop_header">';
+        html += "<h2>" + this.title + "</h2>";
+        html += "</div>";
+      }
 
-		open() {
-			let html = '';
-			html += '<div id="' + this.popId + '" class="commPopup">';
-			if(this.title != null && this.title != ''){
-				html += '<div class="pop_header">';
-				html += '<h2>' + this.title + '</h2>';
-				html += '</div>';
-			}
-			html += '<div class="pop_body">';
-			html += this.msg;
-			html += '</div>';
-			html += '<div class="popup_footer">';
-			html += '<div class="btn_g">';
+      if (this.opt[0] != undefined && Object.keys(this.opt[0]).length != 0) {
+        for (let key in this.opt[0]) {
+          if (key == "style") {
+            html += '<div class="pop_body" style="' + this.opt[0].style + '">';
+          } else if (key == "class") {
+            html += '<div class="pop_body ' + this.opt[0][key] + '">';
+          }
+        }
+      } else {
+        html += '<div class="pop_body">';
+      }
 
-			if (this.type == 'alert') {
-				html += '<button type="button" class="btn btn_small btnCancle" data-popId="' + this.popId + '">닫기</button>';
-			} else {
-				html += '<button type="button" class="btn btn_small btnCancle" data-popId="' + this.popId + '">취소</button>';
-				html += '<button type="button" class="btn btn_small btn_purple btnOk" data-popId="' + this.popId + '">확인</button>';
-			}
-			
-			html += '</div>';
-			html += '</div>';
-			html += '</div>';
-			
-			$('body').append(html);
-			$('body').append('<div class="mask_wrap"></div>');
-			return this;
-		}
+      html += this.msg;
+      html += "</div>";
+      html += '<div class="popup_footer">';
+      html += '<div class="btn_g">';
 
-		alert() {
-			document.querySelector('#' + this.popId + ' .btnCancle').addEventListener('click', this.cancleCallBack);
-			console.log(this);
-			return this;
-		}
+      if (this.type == "alert") {
+        html +=
+          '<button type="button" class="btn btn_small btn_rud30 btn_praimary btnCancle" data-popId="' +
+          this.popId +
+          '">닫기</button>';
+      } else {
+        html +=
+          '<button type="button" class="btn btn_small btn_rud30 btn_praimary btnCancle" data-popId="' +
+          this.popId +
+          '">취소</button>';
+        html +=
+          '<button type="button" class="btn btn_small btn_rud30 btn_praimary btnOk" data-popId="' +
+          this.popId +
+          '">확인</button>';
+      }
 
-		confirm() {
-			document.querySelector('#' + this.popId + ' .btnOk').addEventListener('click', this.okCallBack);
-			document.querySelector('#' + this.popId + ' .btnCancle').addEventListener('click', this.cancleCallBack);
-			return this;
-		}
+      html += "</div>";
+      html += "</div>";
+      html +=
+        '<button type="button" class="btn_close pop_close"><span class="ico">닫기</span></button>';
+      html += "</div>";
 
-		cancle() {
-			$(this).closest('.commPopup').next('.mask_wrap').remove();
-			$(this).closest('.commPopup').remove();
-			console.log('내부', this);
-			return this;
-		}
+      $("body").append(html);
+      $("body").append('<div class="mask_wrap"></div>');
+      return this;
+    }
 
-		ok() {
-			$(this).closest('.commPopup').next('.mask_wrap').remove();
-			$(this).closest('.commPopup').remove();
-			console.log('내부', this);
-			return this;
-		}
-	}
+    alert() {
+      document
+        .querySelector("#" + this.popId + " .btnCancle")
+        .addEventListener("click", this.cancleCallBack);
+      document
+        .querySelector("#" + this.popId + " .btn_close")
+        .addEventListener("click", this.cancle);
+      //   console.log(this);
+      return this;
+    }
 
-	/************************************************************************
+    confirm() {
+      document
+        .querySelector("#" + this.popId + " .btnOk")
+        .addEventListener("click", this.okCallBack);
+      document
+        .querySelector("#" + this.popId + " .btnCancle")
+        .addEventListener("click", this.cancleCallBack);
+      document
+        .querySelector("#" + this.popId + " .btn_close")
+        .addEventListener("click", this.cancle);
+      return this;
+    }
+
+    cancle() {
+      $(this).closest(".commPopup").next(".mask_wrap").remove();
+      $(this).closest(".commPopup").remove();
+      //   console.log("내부", this);
+      return this;
+    }
+
+    ok() {
+      $(this).closest(".commPopup").next(".mask_wrap").remove();
+      $(this).closest(".commPopup").remove();
+      //   console.log("내부", this);
+      return this;
+    }
+  }
+
+  /************************************************************************
 	 [필수] popId : 팝업아이디
 	 [필수] title : 팝업 타이틀 (null, '', 일 시 팝업의 header가 노출 안됨)
 	 [필수] msg : 내용
@@ -78,18 +128,17 @@ const ModalOpen = function (popId, title, msg, type, okFnc, cancleFnc, { ...rest
 	 [필수] cancleFnc : 취소버튼 함수 (빈값으나 null 이라도 보내야함)
 	 {...rest}
 	*************************************************************************/
-	let ModalCreate = new Modal(popId, title, msg, type, okFnc, cancleFnc, { ...rest });
-	ModalCreate.open()[type]();
-}
+  let ModalCreate = new Modal(popId, title, msg, type, okFnc, cancleFnc, {
+    ...rest,
+  });
+  ModalCreate.open()[type]();
+};
 
-let ModalClose = function(obj){
-	let popId = $(obj).attr('data-popid');
-	document.querySelector('#' + popId).nextElementSibling.remove();
-	document.querySelector('#' + popId).remove();
-}
+let ModalClose = function (obj) {
+  let popId = $(obj).attr("data-popid");
+  document.querySelector("#" + popId).nextElementSibling.remove();
+  document.querySelector("#" + popId).remove();
+};
 
 // PopupOpen('firstPopup', '알림1', '내용', 'alert', okClickFnc, cancleClickFnc, {});
 // PopupOpen('firstPopup2', '알림2', '내용', 'confirm', okClickFnc, '', {});
-
-
-
